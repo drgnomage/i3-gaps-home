97f14fd14c9bbf5fa26213bb0977e54fd3dbb8fd
drgnomage <drgnomage@gmail.com>
2016-02-03 22:23:30 +0000

    Changes to be committed: 	new file:   .Xresources 	new file:   .i3/.conkyrc 	new file:   .i3/config 	new file:   .i3/config_dunst 	new file:   .i3/i3lock.convert 	new file:   .i3/rofi.sh 	new file:   .i3/wallpaper_changer.sh.save 	new file:   i3barfodder/config
     Untracked files:
    	HEAD
    	config
    	description
    	hooks/
    	info/
    	objects/

---
+++ .i3/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rofi -show run -lines 3 -eh 2 -width 1800 -padding 50 -opacity "100" -hlfg "#C1BDB4" -hlbg "#6E6A81" -bw 0 -bc "#434460" -bg "#434460" -font "Source Code Pro for Powerline Regular 18" -fg "#B17380" 
